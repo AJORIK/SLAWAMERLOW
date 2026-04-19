2d3e06646096826b75493737943506d656116b3b
AJORIK <160129260+AJORIK@users.noreply.github.com>
2026-04-19 15:26:08 +0300

    main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from fastapi import FastAPI
 from bot import dp, bot
 import uvicorn
@@ -9,7 +10,11 @@ app = FastAPI()
 def root():
     return {"status": "ok"}
 
+async def start_bot():
+    await dp.start_polling(bot)
+
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Railway передает PORT
     loop = asyncio.get_event_loop()
-    loop.create_task(dp.start_polling(bot))
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    loop.create_task(start_bot())
+    uvicorn.run(app, host="0.0.0.0", port=port)
