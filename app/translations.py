8d5bb63f01ab01126149369f46fdcc9f95f22bc5
AJORIK <160129260+AJORIK@users.noreply.github.com>
2026-04-19 14:47:15 +0300

    translations.py

---
+++ app/translations.py
@@ -0,0 +1,12 @@
+texts = {
+    "RU": {
+        "choose_plan": "Привет! Выберите тариф:",
+        "main_menu": "Главное меню:",
+        "payment_created": "Инструкция создана ✅"
+    },
+    "EN": {
+        "choose_plan": "Hi! Choose a plan:",
+        "main_menu": "Main menu:",
+        "payment_created": "Instruction created ✅"
+    }
+}
